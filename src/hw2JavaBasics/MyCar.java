9f446a420fcee0953cf39da6d51f74163345f7f7
shhddin <shhdddin@gmail.com>
2022-05-15 21:38:00 -0400

    updated

--- src/hw2JavaBasics/MyCar.java
+++ src/hw2JavaBasics/MyCar.java
@@ -4,8 +4,12 @@ public class MyCar {
 
 	public static void main(String[] args) {
 		System.out.println("My name is \"Shahed Uddin\"," + " I love to drive," + " My car info is below:");
-		System.out.print("\t Color: Gray, Model:Hyundai Elantra, Year: 2019, \nWheel drive: FWD,");
-		System.out.print(" MPG: 28 city / 37 highway, Engine: 2.0 L 4-cylinder.");
+		System.out.print("\tColor: Gray");
+		System.out.print("\n\tModel:Hyundai Elantra");
+		System.out.print("\n\tYear: 2019");
+		System.out.print("\n\tWheel drive: FWD");
+		System.out.print(" \n\tMPG: 28 city / 37 highway");
+		System.out.print("\n\tEngine: 2.0 L 4-cylinder");
 	}
 
 }
