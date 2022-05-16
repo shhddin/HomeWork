4f807c6a044bc39f779073e6377917ff594a567c
shhddin <shhdddin@gmail.com>
2022-05-16 17:12:32 -0400

    Homework

--- src/hw4JavaVariables/AboutMe.java
+++ src/hw4JavaVariables/AboutMe.java
@@ -12,12 +12,13 @@ public class AboutMe {
 	public char sex;
 	public boolean married;
 
-	//// Constructor Declared 
+	//// Constructor Declared
 	public AboutMe() {
-			System.out.println("\nThis is all about us:\n");
+		System.out.println("\nThis is all about us:\n");
 
 	}
-	// Method is Implemented 
+
+	// Method is Implemented
 	public void aboutMe() {
 		System.out.println("Name: " + name + "\nAge: " + age + "\nSex: " + sex + "\nMarried : " + married + "\nHeight: "
 				+ height + "\nRent: " + rent + "\nSalary: " + salary + "\nBankbalance: " + bankBalance + "\nGPA: "
