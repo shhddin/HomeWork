374c8e279c8091cf6fbd56a47322128efdd9382a
shhddin <shhdddin@gmail.com>
2022-05-25 16:40:30 -0400

    Homework6

---
+++ src/hw6IfElseCondition01/Voter.java
@@ -0,0 +1,17 @@
+package hw6IfElseCondition01;
+
+public class Voter {
+	public static void main(String[] args) {
+		int age = 28;
+
+		if (age == 18) {
+			System.out.println("I am a Voter");
+		} else if (age < 18) {
+			System.out.println("I am not a Voter");
+		} else if (age > 18) {
+			System.out.println("I am a Voter");
+		} else {
+			System.out.println("Please add a valid age");
+		}
+	}
+}
