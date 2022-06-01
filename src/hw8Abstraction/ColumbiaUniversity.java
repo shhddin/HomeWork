928d0b42b3f4b2970fabc8daef0e3cd7b03b0e0c
shhddin <shhdddin@gmail.com>
2022-06-01 17:42:24 -0400

    Regular class ColumbiaUniversity,NYUniversity,RockefellerUniversityis added

---
+++ src/hw8Abstraction/ColumbiaUniversity.java
@@ -0,0 +1,18 @@
+package hw8Abstraction;
+
+public class ColumbiaUniversity {
+
+	// public abstract void ColumbiaUniversity();
+	/*
+	 * The abstract method only be defined by an abstract class
+	 */
+
+	public void biology() {
+		System.out.println("\"biology\" is the non-abstract method of the class \"ColumbiaUniversity\"");
+	}
+
+	public ColumbiaUniversity() {
+
+	}
+
+}
---
+++ src/hw8Abstraction/NYUniversity.java
@@ -0,0 +1,9 @@
+package hw8Abstraction;
+
+public class NYUniversity {
+
+	public void anthropology() {
+		System.out.println("\"anthropology\" is the non-abstract method of the class \"NYUniversity\"");
+	}
+
+}
